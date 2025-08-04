b30d729c5e99e805c427e02d7938c14351ffa5a1
Anthony Salim <tenebralis90@yahoo.com>
2025-08-04 10:10:48 +0700

    add spec for follow_user.rb

--- app/services/follow_user.rb
+++ app/services/follow_user.rb
@@ -5,15 +5,12 @@ class FollowUser
   end
 
   def call
-    puts "11111111"
     raise ActiveRecord::RecordNotFound, 'Follower not found' unless User.exists?(@follower_id)
-    puts "22222222"
     raise ActiveRecord::RecordNotFound, 'Followed user not found' unless User.exists?(@followed_id)
-    puts "33333333"
     follow = Follow.with_deleted.find_by(follower_id: @follower_id, followed_id: @followed_id)
 
     if follow
-      follow.restore! if follow.deleted_at
+      follow.restore! if follow.deleted_at.present?
       follow
     else
       follow = Follow.new(follower_id: @follower_id, followed_id: @followed_id)
